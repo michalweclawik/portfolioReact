23b016f2a584a9ef30291f7e1b476467136c1375
michal <michalwagh@gmail.com>
2023-03-01 19:40:19 +0000

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./componenets/navbar/Navbar";
@@ -6,28 +5,22 @@ import ProjectDetails from "./componenets/projectDetails/ProjectDetails";
 import Home from "./container/Home";
 import ProjectList from "./container/ProjectList";
 import { projects } from "./data/projects";
-import { useNavigate } from "react-router-dom";
+
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("http://michalweclawik.github.io/");
-  }, []);
-
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/portfolioReact" element={<Home />} />
         </Routes>
         <Routes>
           <Route
-            path="/projects"
+            path="/portfolioReact/projects"
             element={<ProjectList projects={projects} />}
           />
           <Route
-            path="/projects/:id"
+            path="/portfolioReact/projects/:id"
             element={<ProjectDetails projects={projects} />}
           ></Route>
         </Routes>
